5475095949cc9212f10b2d152c8185eba5c07723
Tao Lin <47548247+TaoLinShowdown@users.noreply.github.com>
2020-06-27 10:43:10 -0400

    redoing client with vanilla js

--- client/src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React, { useGlobal } from 'reactn';
-
-export default function Counter() {
-    return (
-        <a href="https://github.com/TaoLinShowdown/fibubot">
-            <div>fibubot</div>
-        </a>
-    );
-}
